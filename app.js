84956efde3ae9ced697207faec931b4f88af2bc6
OGSesetti <severi.tahtinen2@gmail.com>
2024-10-24 21:32:15 +0300

    safdae

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+
 var http = require("http");
+const path = require('path')
 
 app.use(express.static('public'));
 // /, /guestbook, /newmessage, /ajaxmessage
 
 
 app.get('/', function(req, res){
-res.sendFile(__dirname + '/public/index.html');
+res.sendFile(path.join(__dirname, 'public', '/index.html'));
 console.log('toimii')
 });
 
 app.get('/guestbook', function(req, res){
-
+    res.sendFile(path.join(__dirname, 'public', '/guestbook.html'));
 });
 
 app.get('/newmessage', function(req, res){
@@ -26,5 +27,5 @@ app.get('/ajaxmessage', function(req, res){
 
 const port = 8081;
 app.listen(port, () =>{
-    console.log('Server running on http://localhost:${port}')
+    console.log('Server running on http://localhost:', port);
 });
